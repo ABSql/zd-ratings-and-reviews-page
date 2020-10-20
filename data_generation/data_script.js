a58184295418e4de5ee25a62399f38f040c8f0d3
Chris Williams <Chris@Chriss-MacBook-Pro-2.local>
2020-10-19 20:19:13 -0600

    adds artillery testing script and data generation functions

--- data_generation/data_script.js
+++ data_generation/data_script.js
@@ -2,9 +2,10 @@ const faker = require('faker')
 const fs = require('fs')
 const fakerFuncs = require('./faker_funcs.js')
 
-const writeReviews = fs.createWriteStream('./10M_Reviews.csv');
+const writeReviews = fs.createWriteStream('./11M_Reviews.csv');
+const testReviews = fs.createWriteStream('./11Mtest_reviews.csv')
 
-const generateXReviews = (writer, number, encoding, cb) => {
+const generateXReviews = (writer, tester, number, encoding, cb) => {
   let i = number
   let id = 1
   let charId = 1
@@ -24,21 +25,29 @@ const generateXReviews = (writer, number, encoding, cb) => {
       id += 1
       if (i <= 0) {
         writer.write(prodEntry, encoding, cb);
+        tester.write(prodEntry, encoding, cb);
       } else {
 // see if we should continue, or wait
 // don't pass the callback, because we're not done yet.
         ok = writer.write(prodEntry, encoding);
+        if (i % 10000 === 0) {
+          ok = tester.write(prodEntry, encoding);
+        }
       }
     } while (i > 0 && ok);
     if (i > 0) {
 // had to stop early!
 // write some more once it drains
       writer.once('drain', write);
+      if (i % 10000 === 0) {
+        tester.once('drain', write);
+      }
     }
   }
 write()
 }
 
-generateXReviews(writeReviews, 10000000, 'utf-8', () => {
+generateXReviews(writeReviews, testReviews, 100000, 'utf-8', () => {
   writeReviews.end()
+  testReviews.end()
 })
